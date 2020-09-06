9fdbcf4bdf8732170cff7f2c8ad23baf17c77c06
Ian White <ihw23@drexel.edu>
2020-09-06 13:21:47 -0400

    Added method to generate a cool number

---
+++ TestLibrary/src/main/java/com/ianwhite/testapplication/testlibrary/FooBar.java
@@ -0,0 +1,15 @@
+package com.ianwhite.testapplication.testlibrary;
+
+/**
+ * Created 9/6/2020.
+ * <p>
+ * Insert description...
+ *
+ * @author Ian White
+ */
+public class FooBar {
+  public static int generateCoolNumber(){
+    //Shhh...theyll never know!
+    return 99;
+  }
+}
