d3eaeccf999d4fd6c987ffe530b06ab64543fb22
Randomguy01 <ianhwhite2001@gmail.com>
2021-06-01 04:13:56 -0400

    Fixed indentation added if without braces
    Signed-off-by: Randomguy01 <ianhwhite2001@gmail.com>

--- app/src/main/java/com/ianwhite/testapplication/libraryapp/MainActivity.java
+++ app/src/main/java/com/ianwhite/testapplication/libraryapp/MainActivity.java
@@ -1,18 +1,22 @@
 package com.ianwhite.testapplication.libraryapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 import com.ianwhite.testapplication.testlibrary.Bar;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-              Bar bar = new Bar();
+    Bar bar = new Bar();
     bar.setMyCoolNumber(99);
-        bar.setMyCoolerNumber(999);
+    bar.setMyCoolerNumber(999);
+
+    if (bar.getMyCoolNumber() == 99)
+      Log.d("HI", "I'm 99");
   }
 }
